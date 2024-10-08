1f8f85f981fba8e36bd9d908165c5031d93bf790
Gugolivieri <olivieri.guglielmo2@gmail.com>
2024-10-08 16:37:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-const countdownDate = new Date("October 10, 2024 00:00:00").getTime();
+const countdownDate = new Date("October 10, 2024 21:00:00").getTime();
 
 // Update the countdown every 1 second
 const countdownFunction = setInterval(function() {
